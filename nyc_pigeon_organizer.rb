480b345e1c5a8bc9a31e29abb06d165f7e6f2eb8
Jeffrey Yu <jwy5140@gmail.com>
2019-03-07 22:00:23 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,27 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  ans = Hash.new
+  
+  data_1d = data.flatten
+  data_1d.each {|x|
+    if x.is_a?(Symbol)
+      next
+    elsif x.is_a?(Hash)
+      x.each {|desc,array|
+        array.each {|name|
+          if !ans[name]
+            ans[name] = {}
+          end 
+          if !ans[name][x]
+            ans[name][x] = []
+          elsif !ans[name][x].include?(desc.to_s)
+            ans[name][x] << desc.to_s
+          end
+        }
+      }
+    end
+  }
+  ans
+  binding.pry
 end
